86b2ed23486cb7a9b645201f6603e3b059e38120
Micah <micahshallom@gmail.com>
2024-02-13 04:50:33 +0100

    added terraform installation to install.sh

--- build/install.sh
+++ build/install.sh
@@ -1,25 +1,35 @@
 !#/bin/bash
 
+#kind installation
 [ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-amd64
-
 sudo chmod +x ./kind
-
 sudo mv ./kind /usr/local/bin/kind
 
+#docker installation
 sudo apt update -y
-
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
-
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-
 sudo apt-cache policy docker-ce
-
 sudo apt install docker-ce
-
 sudo usermod -aG docker ${USER}
 
+#kubect installation
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo chmod +x kubectl
 sudo mv kubectl /usr/local/bin/kubectl
+
+#terraform  installation
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+gpg --dearmor | \
+sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+gpg --no-default-keyring \
+--keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+--fingerprint
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update -y
+sudo apt-get install terraform
+terraform -help
